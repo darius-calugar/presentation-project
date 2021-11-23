00065281bfe93bdb2d807ca9cd1105cdcf71e5d2
Darius Calugar <darius_calugar@yahoo.com>
2021-11-24 01:54:24 +0200

    Implemented volume-aware squishing

--- Assets/Scripts/PlayerVisual.cs
+++ Assets/Scripts/PlayerVisual.cs
@@ -7,6 +7,7 @@ public class PlayerVisual : MonoBehaviour
 	[SerializeField] [Range(0, 1f)] private float            movingAverageForce;
 	[SerializeField] [Min(0)]       private float            accelerationWeight;
 	[SerializeField] [Min(0)]       private float            accelerationClamp;
+	[SerializeField] [Min(0)]       private float            currentVelocityWeight;
 
 	private Rigidbody _playerRigidbody;
 
@@ -26,7 +27,10 @@ public class PlayerVisual : MonoBehaviour
 		_movingAverage = _movingAverage * (1 - movingAverageForce) +
 						 Math.Min(Math.Max(acceleration * accelerationWeight, -accelerationClamp), accelerationClamp) * movingAverageForce;
 
-		transform.localScale = Vector3.one + Vector3.up * _movingAverage;
-		transform.rotation   = Quaternion.LookRotation(Vector3.forward, currentVelocity);
+		transform.localScale =
+			Vector3.one +
+			Vector3.up * (_movingAverage + currentVelocity.magnitude * currentVelocityWeight) +
+			Vector3.left * _movingAverage;
+		transform.rotation = Quaternion.LookRotation(Vector3.forward, currentVelocity);
 	}
 }
